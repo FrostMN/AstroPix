24b1106d5e7a7f5488102909802f384df8fda972
aaron <asouer@gmail.com>
2017-11-21 14:19:16 -0600

    favs

--- routes/favorites.js
+++ routes/favorites.js
@@ -1,29 +1,33 @@
 var express = require('express');
-var router = express.Router;
+var router = express.Router();
 
-/* Get Fave Page */
-router.get('/', function (req, res, next){
-    res.render('favorites', { favorites: req.session.favorites });
+/* GET favorites page */
+router.get('/', function(req, res, next){
+    res.render('favorites', {favorites : req.session.favorites});
 });
 
-router.post('/add', function (req, res, next){
-   if (!req.session.favorites) {
-       req.session.favorites = [];
-   }
 
-   // Is this Image already a fave
-    var isFave = false;
-   for (var i = 0; i < req.session.favorites.length; i++) {
-       if (req.session.favorites[i].date == req.body.date) {
-           isFave = true;
-       }
-   }
+router.post('/add', function(req, res, next){
 
-   if (!isFave) {
-       req.session.favorites.push(req.body)
-   }
+    // Create a favorites array in the session, if it does not exist
+    if (!req.session.favorites) {
+        req.session.favorites = [];
+    }
 
-   res.redirect('/favorites');
+    // Is this image already a favorite? Ignore
+    var isFav = false;
+    for (var i = 0 ; i < req.session.favorites.length; i++) {
+        if (req.session.favorites[i].date == req.body.date) {
+            // already in the array. Redirect.
+            isFav = true;  break;
+        }
+    }
+
+    if (!isFav) {
+        // Add all the info about an image to req.session.favorites
+        req.session.favorites.push(req.body);
+    }
+    res.redirect('/favorites');
 
 });
 
